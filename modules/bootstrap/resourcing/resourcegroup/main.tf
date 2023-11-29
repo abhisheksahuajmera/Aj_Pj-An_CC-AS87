7763b03b3e7f1e35f31cc148eaedfec82ad0e15a
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-29 19:05:28 +0530

    Resolved 109

--- modules/bootstrap/resourcing/resourcegroup/main.tf
+++ modules/bootstrap/resourcing/resourcegroup/main.tf
@@ -1,16 +1,11 @@
 variable "rg_version" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "100"
+  default     = "101"
 }
 
 // Resource-Group
-# resource "azurerm_resource_group" "${var.prefixop}-${var.environmentop}-${var.environment_versionop}-${var.rg_versionop}tg" {
-#   name     = "var.prefixop-var.environmentop-${var.environment_versionop}-${var.rg_versionop}"
-#   location = "east us"
-# }
-
-resource "azurerm_resource_group" "argtg" {
-  name     = "var.prefixop"
+resource "azurerm_resource_group" "${var.prefixop}-${var.environmentop}-${var.environment_versionop}-${var.rg_versionop}tg" {
+  name     = "${var.prefixop}-${var.environmentop}-${var.environment_versionop}-${var.rg_versionop}"
   location = "east us"
 }
