ccf44709328966f393c76f7464a61a9004032727
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-29 18:48:10 +0530

    Resolve 105

--- modules/bootstrap/resourcing/resourcegroup/main.tf
+++ modules/bootstrap/resourcing/resourcegroup/main.tf
@@ -1,3 +1,9 @@
+locals {
+ prefixop  = var.prefixop,
+// prefixop  = ${var.prefixop},
+ prefixopmy  = "prefixopmy",
+}
+ 
 variable "rg_version" {
   type        = string
   description = "(Required) Update to force a change."
@@ -5,7 +11,12 @@ variable "rg_version" {
 }
 
 // Resource-Group
-resource "azurerm_resource_group" "${var.prefixop}-${var.environmentop}-${var.environment_versionop}-${var.rg_versionop}tg" {
-  name     = "${var.prefixop}-${var.environmentop}-${var.environment_versionop}-${var.rg_versionop}"
+// resource "azurerm_resource_group" "${var.prefixop}-${var.environmentop}-${var.environment_versionop}-${var.rg_versionop}tg" {
+//  name     = "var.prefixop-var.environmentop-${var.environment_versionop}-${var.rg_versionop}"
+//  location = "east us"
+// }
+
+resource "azurerm_resource_group" "argtg" {
+  name     = "var.prefixop"
   location = "east us"
 }
