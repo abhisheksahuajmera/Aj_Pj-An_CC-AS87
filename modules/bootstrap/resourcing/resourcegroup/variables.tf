b1dbd93543245ff6f1370c377239882b41affbce
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-30 04:36:27 +0530

    Resolve 105

--- modules/bootstrap/resourcing/resourcegroup/variables.tf
+++ modules/bootstrap/resourcing/resourcegroup/variables.tf
@@ -22,6 +22,12 @@ variable "resource_group_version" {
   default     = "100"
 }
 
+variable "resource_group_prefix" {
+  type        = string
+  description = "(Required) Update to force a change."
+  default     = "rg"
+}
+
 variable "resource_group_location" {
   type        = string
   description = "(Required) Update to force a change."
@@ -29,6 +35,6 @@ variable "resource_group_location" {
 }
 
 locals {
-  resource_group_name    = "${var.prefix}-${var.environment}-${var.environment_version}-${var.resource_group_version}"
+  resource_group_name    = "${var.prefix}-${var.environment}-${var.environment_version}-${var.resource_group_prefix}-${var.resource_group_version}"
   resource_group_location = "${var.resource_group_location}"
 }
