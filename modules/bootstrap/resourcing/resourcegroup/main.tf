b550ea30f3850d9ebbbf258ac380d10d9bcd67d6
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-29 19:27:29 +0530

    Resolve 111

--- modules/bootstrap/resourcing/resourcegroup/main.tf
+++ modules/bootstrap/resourcing/resourcegroup/main.tf
@@ -4,13 +4,7 @@ variable "rg_version" {
   default     = "100"
 }
 
-// Resource-Group
-# resource "azurerm_resource_group" "${var.prefixop}-${var.environmentop}-${var.environment_versionop}-${var.rg_versionop}tg" {
-#   name     = "var.prefixop-var.environmentop-var.environment_versionop-var.rg_versionop"
-#   location = "east us"
-# }
-
-resource "azurerm_resource_group" "argtg" {
+resource "azurerm_resource_group" "var.prefixop-var.environmentop-var.environment_versionop-var.rg_versionoptg" {
   name     = "var.prefixop-var.environmentop-var.environment_versionop-var.rg_versionop"
   location = "east us"
 }
