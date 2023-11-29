1f9a5d72a2b2e9d8a88a4f2a289d02eec18f253b
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-29 16:43:04 +0530

    Step 03 : Create ResourceGroup and ADB2C Directory

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ provider "azurerm" {
 }
 
 // Resource-Group
-# resource "azurerm_resource_group" "rg100tg" {
-#   name     = "rg100"
-#   location = "east us"
-# }
+resource "azurerm_resource_group" "rg101tg" {
+  name     = "rg101"
+  location = "east us"
+}
 
 // Resource
 resource "azurerm_aadb2c_directory" "adb2c101tg" {
@@ -25,6 +25,6 @@ resource "azurerm_aadb2c_directory" "adb2c101tg" {
   data_residency_location = "United States"
   display_name            = "adb2c101"
   domain_name             = "adb2c101.onmicrosoft.com"
-  resource_group_name     = "rg100"
+  resource_group_name     = "rg101"
   sku_name                = "PremiumP1"
 }
