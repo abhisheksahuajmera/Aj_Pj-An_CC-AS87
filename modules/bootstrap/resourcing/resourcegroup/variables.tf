5ef4325a28c1858f864ea56ff57e9830147ee503
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-30 04:27:45 +0530

    Resolve 103

--- modules/bootstrap/resourcing/resourcegroup/variables.tf
+++ modules/bootstrap/resourcing/resourcegroup/variables.tf
@@ -4,6 +4,13 @@ variable "resource_group_version" {
   default     = "100"
 }
 
+variable "resource_group_location" {
+  type        = string
+  description = "(Required) Update to force a change."
+  default     = "east us"
+}
+
 locals {
-  resource_group_name    = "${var.prefixop}-${var.environmentop}-${var.environment_versionop}-${var.resource_group_version}"
+  resource_group_name    = "${var.prefix}-${var.environment}-${var.environment_version}-${var.resource_group_version}"
+  resource_group_location = "${var.resource_group_location}"
 }
