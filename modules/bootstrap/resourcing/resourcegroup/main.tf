e3013d56b073f338394cdc070e825c847faf9565
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-30 04:08:59 +0530

    Resolve 100

--- modules/bootstrap/resourcing/resourcegroup/main.tf
+++ modules/bootstrap/resourcing/resourcegroup/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = var.prefixop-var.environmentop-var.environment_versionop-var.resource_group_version" //"${resource_group_name}" 
+  name     = ${resource_group_name}" //"var.prefixop-var.environmentop-var.environment_versionop-var.resource_group_version" 
   location = "east us"
 }
