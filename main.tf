fb77ec60df08306d4421ec864e79c5f36b34e388
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-29 18:16:58 +0530

    Replaced with env var

--- main.tf
+++ main.tf
@@ -13,8 +13,14 @@ provider "azurerm" {
   features {}
 }
 
+variable "rg_version" {
+  type        = string
+  description = "(Required) Update to force a change."
+  default     = "100"
+}
+
 // Resource-Group
 resource "azurerm_resource_group" "assetronai-dev-100tg" {
-  name     = "assetronai-dev-100"
+  name     = "${var.prefix}-${var.environment}-${var.environment_version}-${var.rg_version}" //"assetronai-dev-100"
   location = "east us"
 }
