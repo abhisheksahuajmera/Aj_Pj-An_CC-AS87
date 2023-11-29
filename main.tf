58266cd611595e622418d9ce9caed04f58dad0fd
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-11-30 04:48:18 +0530

    Try 101

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
+// Can copy rootmodule-.tf var file to submodule or pass the variables
 module "resourcing" {
-  source  = "./modules/bootstrap/resourcing/resourcegroup" // Add version after registry
-  
-  // Can copy rootmodule-.tf var file to submodule or pass the variables
+  source  = "./modules/bootstrap/resourcing/resourcegroup" // Add version after registry 
   prefix = "${var.prefix}"
   environment = "${var.environment}"
   environment_version = "${var.environment_version}"
